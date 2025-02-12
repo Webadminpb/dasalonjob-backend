e5ded89cf7d635da48fc12cd2ac1d47dc41528a9
sahiljaggarwal <sahiljaggrwal@gmail.com>
2025-02-12 16:43:22 +0530

    agency partner

--- src/modules/agency-partner-salons/agency-partner-salons.module.ts
+++ src/modules/agency-partner-salons/agency-partner-salons.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AgencyPartnerSalonsService } from './agency-partner-salons.service';
 import { AgencyPartnerSalonsController } from './agency-partner-salons.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AgencyPartnerSalonsController],
-  providers: [AgencyPartnerSalonsService],
+  providers: [AgencyPartnerSalonsService, PrismaService, JwtService],
 })
 export class AgencyPartnerSalonsModule {}
