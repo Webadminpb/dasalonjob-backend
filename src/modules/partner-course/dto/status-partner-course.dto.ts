aa2f294d5daa934063346cf33d06c059e8bb54fe
sahiljaggarwal <sahiljaggrwal@gmail.com>
2025-04-03 17:17:00 +0530

    status changed

--- src/modules/partner-course/dto/status-partner-course.dto.ts
+++ src/modules/partner-course/dto/status-partner-course.dto.ts
@@ -1,10 +1,10 @@
-import { JobPostStatus } from '@prisma/client';
+import { CourseStatus, JobPostStatus } from '@prisma/client';
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const CourseStatusSchema = z.object({
   id: z.string(),
-  status: z.nativeEnum(JobPostStatus),
+  status: z.nativeEnum(CourseStatus),
 });
 
 export class CreateCourseStatusDto extends createZodDto(CourseStatusSchema) {}
